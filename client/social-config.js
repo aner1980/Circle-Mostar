2177f24a40f8640812628f1e3e56e7ddeebab795
Derek <dadewitt@purdue.edu>
2015-11-05 14:52:59 -0500

    Manny work

---
+++ client/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '812411045548823',
+    secret: 'cd7c9e4fd723e04c0838a7b91aaf2c14'
+});
