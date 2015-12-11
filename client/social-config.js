f4e8423d6a0760fce9e2faac2424565c883feec1
Derek <dadewitt@purdue.edu>
2015-12-11 02:54:23 -0500

    Facebook/Google login + Fix leaderboard

--- client/social-config.js
+++ client/social-config.js
@@ -7,3 +7,14 @@ ServiceConfiguration.configurations.insert({
     appId: '812411045548823',
     secret: 'cd7c9e4fd723e04c0838a7b91aaf2c14'
 });
+
+
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "939025466903-d8gbrfdk50d30613ma0uhs1bbbo5s44r.apps.googleusercontent.com",
+  secret: "tM_e1DdNWiKXQ9aiV7WboM5u"
+});
+
